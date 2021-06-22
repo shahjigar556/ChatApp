e8e27e24e689c306d9dc1ea9edd48268d1770d1c
Jigar <shahjigar185@gmail.com>
2021-06-22 23:58:52 +0530

    Intialized firebase

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/welcome_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/chat_screen.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:WelcomeScreen(),
+      routes:{
+    ChatScreen.id: (BuildContext context) => ChatScreen(),
+    LoginScreen.id: (BuildContext context) => LoginScreen(),
+    RegistrationScreen.id: (BuildContext context) => RegistrationScreen(),
+    }
+    );
+  }
+}
+
+
+
---
+++ lib/screens/registration_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class RegistrationScreen extends StatefulWidget {
+  static String id='registration_screen';
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
